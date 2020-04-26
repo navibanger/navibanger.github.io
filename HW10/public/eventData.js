49dd0441d34c7943411fa5115718961eec1041c8
Navneet Banger <navneetbanger95@gmail.com>
2020-04-26 14:20:44 -0700

    test

--- HW10/public/eventData.js
+++
@@ -1,5 +0,0 @@
-events = [
-    {"name": "Pizza Day", "dates": ["August 1", " August 3"]},
-    {"name": "BBQ Day", "dates": ["August 5", " August 7"]},
-    {"name": "Ice Cream Day", "dates": ["August 9", " August 11"]}
-];
